c08bde876498a114a7cf1aa660edcff4a09820e1
miku <michael.jaser@peerigon.com>
2013-04-08 22:28:18 +0200

    - Grundstruktur & "Einführung"

---
+++ lectures/einfuehrung/examples/code-reuse/bundle.js
@@ -0,0 +1,35 @@
+/******/ (function webpackBootstrap(modules) {
+/******/ 	var installedModules = {};
+/******/ 	function require(moduleId) {
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/ 		modules[moduleId].call(null, module, module.exports, require);
+/******/ 		module.loaded = true;
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	require.e = function requireEnsure(chunkId, callback) {
+/******/ 		callback.call(null, require);
+/******/ 	};
+/******/ 	require.modules = modules;
+/******/ 	require.cache = installedModules;
+/******/ 	return require(0);
+/******/ })({
+/******/ c: "",
+
+/***/ 0:
+/***/ function(module, exports, require) {
+
+	
+	function printHelloWorld() {
+	    console.log("Hello World");
+	}
+	
+	module.exports = printHelloWorld();
+
+/***/ }
+/******/ })
