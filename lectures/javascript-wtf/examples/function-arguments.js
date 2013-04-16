3a4548bf3bfec785bbd6fd7c500c4fdf4cbfb4b1
jhnns <johannes.ewald@peerigon.com>
2013-04-16 14:03:31 +0200

    - added small fixes

--- lectures/javascript-wtf/examples/function-arguments.js
+++ lectures/javascript-wtf/examples/function-arguments.js
@@ -1,6 +1,9 @@
 function myFunction(firstArg, secondArg, thirdArg) {
+    var args;
+
     console.log(firstArg, secondArg, thirdArg);
     console.log(arguments);
+    args = Array.prototype.slice.call(arguments, 0);
 }
 
 myFunction("a", "b", "c", "d");
