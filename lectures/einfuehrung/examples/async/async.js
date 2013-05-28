0d530bcdea694b7907754b493b7f270e85f608a0
miku <michael.jaser@peerigon.com>
2013-05-28 15:12:11 +0200

    best practices

---
+++ lectures/einfuehrung/examples/async/async.js
@@ -0,0 +1,12 @@
+var fs = require("fs");
+
+console.log("Hello!");
+
+var names = {};
+
+fs.readFile("./names.json", function(err, namesJSON) {
+    names = JSON.parse(namesJSON);
+    console.log("parsing done");
+});
+
+console.log("We've got " + Object.keys(names).length + " names in memory");
---
+++ lectures/nodejs-basics/examples/user-module/myModule/README.md
@@ -0,0 +1,3 @@
+## myModule
+
+__README.md__ is the right place for documentation and a description of your module.
